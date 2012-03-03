0b3e90519cd6b1d814324755089425fe2eb1c52d
Alex Parrill <initrd.gz@gmail.com>
2012-03-02 22:31:25 -0600

    Added support for generating premutation tables.

--- simplexnoise.c
+++ simplexnoise.c
@@ -34,58 +34,33 @@
 
 
 #include	"simplexnoise.h"
+#include	"mt19937ar.c"
 
 #define FASTFLOOR(x) ( ((x)>0) ? ((int)x) : (((int)x)-1) )
 
 //---------------------------------------------------------------------
-// Static data
 
-/*
- * Permutation table. This is just a random jumble of all numbers 0-255,
- * repeated twice to avoid wrapping the index at 255 for each lookup.
- * This needs to be exactly the same for all instances on all platforms,
- * so it's easiest to just keep it as static explicit data.
- * This also removes the need for any initialisation of this class.
- *
- * Note that making this an int[] instead of a char[] might make the
- * code run faster on platforms with a high penalty for unaligned single
- * byte addressing. Intel x86 is generally single-byte-friendly, but
- * some other CPUs are faster with 4-aligned reads.
- * However, a char[] is smaller, which avoids cache trashing, and that
- * is probably the most important aspect on most architectures.
- * This array is accessed a *lot* by the noise functions.
- * A vector-valued noise over 3D accesses it 96 times, and a
- * float-valued 4D noise 64 times. We want this to fit in the cache!
- */
-unsigned char perm[512] = {151,160,137,91,90,15,
-  131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,
-  190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,
-  88,237,149,56,87,174,20,125,136,171,168, 68,175,74,165,71,134,139,48,27,166,
-  77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,55,46,245,40,244,
-  102,143,54, 65,25,63,161, 1,216,80,73,209,76,132,187,208, 89,18,169,200,196,
-  135,130,116,188,159,86,164,100,109,198,173,186, 3,64,52,217,226,250,124,123,
-  5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,189,28,42,
-  223,183,170,213,119,248,152, 2,44,154,163, 70,221,153,101,155,167, 43,172,9,
-  129,22,39,253, 19,98,108,110,79,113,224,232,178,185, 112,104,218,246,97,228,
-  251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,107,
-  49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
-  138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180,
-  151,160,137,91,90,15,
-  131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,
-  190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,
-  88,237,149,56,87,174,20,125,136,171,168, 68,175,74,165,71,134,139,48,27,166,
-  77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,55,46,245,40,244,
-  102,143,54, 65,25,63,161, 1,216,80,73,209,76,132,187,208, 89,18,169,200,196,
-  135,130,116,188,159,86,164,100,109,198,173,186, 3,64,52,217,226,250,124,123,
-  5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,189,28,42,
-  223,183,170,213,119,248,152, 2,44,154,163, 70,221,153,101,155,167, 43,172,9,
-  129,22,39,253, 19,98,108,110,79,113,224,232,178,185, 112,104,218,246,97,228,
-  251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,107,
-  49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
-  138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180 
-};
+// Sets up a premutation table using the given seed.
+void snoise_setup_perm(snoise_permtable* p, unsigned long seed)
+{
+	mt_init_genrand(seed);
+	snoise_setup_perm_noseed(p);
+}
 
-//---------------------------------------------------------------------
+// Sets up a premutation table, not setting any seed. Useful with mt_init_by_array
+void snoise_setup_perm_noseed(snoise_permtable* p)
+{
+	int i, j, k;
+	
+	for (i = 0 ; i < 256 ; i++)
+		p->perm[i] = i;
+	while (--i)
+	{
+		k = p->perm[i];
+		p->perm[i] = p->perm[j = (unsigned char)(mt_genrand_int32() & 0xff)];
+		p->perm[j] = k;
+	}
+}
 
 /*
  * Helper functions to compute gradients-dot-residualvectors (1D to 4D)
@@ -143,7 +118,7 @@ static float grad4( int hash, float x, float y, float z, float t ) {
     {2,1,0,3},{0,0,0,0},{0,0,0,0},{0,0,0,0},{3,1,0,2},{0,0,0,0},{3,2,0,1},{3,2,1,0}};
 
 // 1D simplex noise
-float snoise1(float x) {
+float snoise1(snoise_permtable* permtable, float x) {
 
   int i0 = FASTFLOOR(x);
   int i1 = i0 + 1;
@@ -155,12 +130,12 @@ float snoise1(float x) {
   float t0 = 1.0f - x0*x0;
 //  if(t0 < 0.0f) t0 = 0.0f;
   t0 *= t0;
-  n0 = t0 * t0 * grad1(perm[i0 & 0xff], x0);
+  n0 = t0 * t0 * grad1(permtable->perm[i0 & 0xff], x0);
 
   float t1 = 1.0f - x1*x1;
 //  if(t1 < 0.0f) t1 = 0.0f;
   t1 *= t1;
-  n1 = t1 * t1 * grad1(perm[i1 & 0xff], x1);
+  n1 = t1 * t1 * grad1(permtable->perm[i1 & 0xff], x1);
   // The maximum value of this noise is 8*(3/4)^4 = 2.53125
   // A factor of 0.395 would scale to fit exactly within [-1,1], but
   // we want to match PRMan's 1D noise, so we scale it down some more.
@@ -169,7 +144,7 @@ float snoise1(float x) {
 }
 
 // 2D simplex noise
-float snoise2(float x, float y) {
+float snoise2(snoise_permtable* permtable, float x, float y) {
 
 #define F2 0.366025403 // F2 = 0.5*(sqrt(3.0)-1.0)
 #define G2 0.211324865 // G2 = (3.0-Math.sqrt(3.0))/6.0
@@ -213,21 +188,21 @@ float snoise2(float x, float y) {
     if(t0 < 0.0f) n0 = 0.0f;
     else {
       t0 *= t0;
-      n0 = t0 * t0 * grad2(perm[ii+perm[jj]], x0, y0); 
+      n0 = t0 * t0 * grad2(permtable->perm[ii+permtable->perm[jj]], x0, y0); 
     }
 
     float t1 = 0.5f - x1*x1-y1*y1;
     if(t1 < 0.0f) n1 = 0.0f;
     else {
       t1 *= t1;
-      n1 = t1 * t1 * grad2(perm[ii+i1+perm[jj+j1]], x1, y1);
+      n1 = t1 * t1 * grad2(permtable->perm[ii+i1+permtable->perm[jj+j1]], x1, y1);
     }
 
     float t2 = 0.5f - x2*x2-y2*y2;
     if(t2 < 0.0f) n2 = 0.0f;
     else {
       t2 *= t2;
-      n2 = t2 * t2 * grad2(perm[ii+1+perm[jj+1]], x2, y2);
+      n2 = t2 * t2 * grad2(permtable->perm[ii+1+permtable->perm[jj+1]], x2, y2);
     }
 
     // Add contributions from each corner to get the final noise value.
@@ -236,7 +211,7 @@ float snoise2(float x, float y) {
   }
 
 // 3D simplex noise
-float snoise3(float x, float y, float z) {
+float snoise3(snoise_permtable* permtable, float x, float y, float z) {
 
 // Simple skewing factors for the 3D case
 #define F3 0.333333333
@@ -304,28 +279,28 @@ float snoise3(float x, float y, float z) {
     if(t0 < 0.0f) n0 = 0.0f;
     else {
       t0 *= t0;
-      n0 = t0 * t0 * grad3(perm[ii+perm[jj+perm[kk]]], x0, y0, z0);
+      n0 = t0 * t0 * grad3(permtable->perm[ii+permtable->perm[jj+permtable->perm[kk]]], x0, y0, z0);
     }
 
     float t1 = 0.6f - x1*x1 - y1*y1 - z1*z1;
     if(t1 < 0.0f) n1 = 0.0f;
     else {
       t1 *= t1;
-      n1 = t1 * t1 * grad3(perm[ii+i1+perm[jj+j1+perm[kk+k1]]], x1, y1, z1);
+      n1 = t1 * t1 * grad3(permtable->perm[ii+i1+permtable->perm[jj+j1+permtable->perm[kk+k1]]], x1, y1, z1);
     }
 
     float t2 = 0.6f - x2*x2 - y2*y2 - z2*z2;
     if(t2 < 0.0f) n2 = 0.0f;
     else {
       t2 *= t2;
-      n2 = t2 * t2 * grad3(perm[ii+i2+perm[jj+j2+perm[kk+k2]]], x2, y2, z2);
+      n2 = t2 * t2 * grad3(permtable->perm[ii+i2+permtable->perm[jj+j2+permtable->perm[kk+k2]]], x2, y2, z2);
     }
 
     float t3 = 0.6f - x3*x3 - y3*y3 - z3*z3;
     if(t3<0.0f) n3 = 0.0f;
     else {
       t3 *= t3;
-      n3 = t3 * t3 * grad3(perm[ii+1+perm[jj+1+perm[kk+1]]], x3, y3, z3);
+      n3 = t3 * t3 * grad3(permtable->perm[ii+1+permtable->perm[jj+1+permtable->perm[kk+1]]], x3, y3, z3);
     }
 
     // Add contributions from each corner to get the final noise value.
@@ -335,7 +310,7 @@ float snoise3(float x, float y, float z) {
 
 
 // 4D simplex noise
-float snoise4(float x, float y, float z, float w) {
+float snoise4(snoise_permtable* permtable, float x, float y, float z, float w) {
   
   // The skewing and unskewing factors are hairy again for the 4D case
 #define F4 0.309016994 // F4 = (Math.sqrt(5.0)-1.0)/4.0
@@ -434,35 +409,35 @@ float snoise4(float x, float y, float z, float w) {
     if(t0 < 0.0f) n0 = 0.0f;
     else {
       t0 *= t0;
-      n0 = t0 * t0 * grad4(perm[ii+perm[jj+perm[kk+perm[ll]]]], x0, y0, z0, w0);
+      n0 = t0 * t0 * grad4(permtable->perm[ii+permtable->perm[jj+permtable->perm[kk+permtable->perm[ll]]]], x0, y0, z0, w0);
     }
 
    float t1 = 0.6f - x1*x1 - y1*y1 - z1*z1 - w1*w1;
     if(t1 < 0.0f) n1 = 0.0f;
     else {
       t1 *= t1;
-      n1 = t1 * t1 * grad4(perm[ii+i1+perm[jj+j1+perm[kk+k1+perm[ll+l1]]]], x1, y1, z1, w1);
+      n1 = t1 * t1 * grad4(permtable->perm[ii+i1+permtable->perm[jj+j1+permtable->perm[kk+k1+permtable->perm[ll+l1]]]], x1, y1, z1, w1);
     }
 
    float t2 = 0.6f - x2*x2 - y2*y2 - z2*z2 - w2*w2;
     if(t2 < 0.0f) n2 = 0.0f;
     else {
       t2 *= t2;
-      n2 = t2 * t2 * grad4(perm[ii+i2+perm[jj+j2+perm[kk+k2+perm[ll+l2]]]], x2, y2, z2, w2);
+      n2 = t2 * t2 * grad4(permtable->perm[ii+i2+permtable->perm[jj+j2+permtable->perm[kk+k2+permtable->perm[ll+l2]]]], x2, y2, z2, w2);
     }
 
    float t3 = 0.6f - x3*x3 - y3*y3 - z3*z3 - w3*w3;
     if(t3 < 0.0f) n3 = 0.0f;
     else {
       t3 *= t3;
-      n3 = t3 * t3 * grad4(perm[ii+i3+perm[jj+j3+perm[kk+k3+perm[ll+l3]]]], x3, y3, z3, w3);
+      n3 = t3 * t3 * grad4(permtable->perm[ii+i3+permtable->perm[jj+j3+permtable->perm[kk+k3+permtable->perm[ll+l3]]]], x3, y3, z3, w3);
     }
 
    float t4 = 0.6f - x4*x4 - y4*y4 - z4*z4 - w4*w4;
     if(t4 < 0.0f) n4 = 0.0f;
     else {
       t4 *= t4;
-      n4 = t4 * t4 * grad4(perm[ii+1+perm[jj+1+perm[kk+1+perm[ll+1]]]], x4, y4, z4, w4);
+      n4 = t4 * t4 * grad4(permtable->perm[ii+1+permtable->perm[jj+1+permtable->perm[kk+1+permtable->perm[ll+1]]]], x4, y4, z4, w4);
     }
 
     // Sum up and scale the result to cover the range [-1,1]
--- simplexnoise.h
+++ simplexnoise.h
@@ -29,8 +29,22 @@
  * on some platforms. A templatized version of SimplexNoise1234 could be useful.
  */
 
-float snoise1(float x);
-float snoise2(float x, float y);
-float snoise3(float x, float y, float z);
-float snoise4(float x, float y, float z, float w);
+typedef struct {
+	unsigned char perm[512];
+} snoise_permtable;
 
+void snoise_setup_perm(snoise_permtable* p, unsigned long seed);
+void snoise_setup_perm_noseed(snoise_permtable* p);
+float snoise1(snoise_permtable* c, float x);
+float snoise2(snoise_permtable* c, float x, float y);
+float snoise3(snoise_permtable* c, float x, float y, float z);
+float snoise4(snoise_permtable* c, float x, float y, float z, float w);
+
+void mt_init_genrand(unsigned long s);
+void mt_init_by_array(unsigned long init_key[], int key_length);
+unsigned long mt_genrand_int32(void);
+long mt_genrand_int31(void);
+double mt_genrand_real1(void);
+double mt_genrand_real2(void);
+double mt_genrand_real3(void);
+double mt_genrand_res53(void);
